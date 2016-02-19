ed4f4d5d60b30c07b61f8479f9aa269e095f0c54
Gert Leenders <leenders.gert@gmail.com>
2016-02-19 08:30:51 +0100

    Fixed policy, added security group

--- vpc.tf
+++ vpc.tf
@@ -20,3 +20,51 @@ resource "aws_subnet" "oly_public_1a" {
     Name = "oly_public_1a"
   }
 }
+
+resource "aws_iam_role" "ecsInstanceRole" {
+  name = "ecsInstanceRole"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy_attachment" "AmazonEC2ContainerServiceforEC2Role" {
+    name = "AmazonEC2ContainerServiceforEC2Role"
+    roles = ["${aws_iam_role.ecsInstanceRole.name}"]
+    policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+resource "aws_security_group" "allow_all" {
+  name = "allow_all"
+  description = "allow all traffic"
+  vpc_id = "${aws_vpc.olympus.id}"
+
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "allow_all"
+  }
+}
