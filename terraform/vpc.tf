e5374d3bbf5489a01aa155b99c919f5874fe3b25
Gert Leenders <leenders.gert@gmail.com>
2016-05-12 13:55:46 +0200

    Fixed internet routing

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -27,6 +27,22 @@ resource "aws_subnet" "oly_public_1a" {
   }
 }
 
+resource "aws_route_table" "rt_public_oly" {
+  vpc_id = "${aws_vpc.olympus.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.oly_gw.id}"
+  }
+  tags {
+    Name = "rt_public_oly"
+  }
+}
+
+resource "aws_route_table_association" "association" {
+  subnet_id = "${aws_subnet.oly_public_1a.id}"
+  route_table_id = "${aws_route_table.rt_public_oly.id}"
+}
+
 resource "aws_security_group" "allow_all" {
   name = "allow_all"
   description = "allow all traffic"
